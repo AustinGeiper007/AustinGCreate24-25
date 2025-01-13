c0954e006da1b009c04b0e25a13ae21262d98063
AustinG <austin.geisler@vtsd.com>
2025-01-13 12:28:19 -0500

    filters are working ish (send help I forgor regex)

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import math as math
 import turtle as trtl
 import re
-from lib2to3.pgen2.literals import evalString
 
-equation = input("Enter equation: ")
-filter = '/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/'
+input = input('Input equation: ')
+filter = 'sin\('
 
-equation = re.findall(filter, equation)
+equation = re.sub(filter, 'math.sin(', input)
+
 
 print(equation)
