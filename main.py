2afb1a177e6f90a95067f9a6bccfe0755ae7545c
AustinG <austin.geisler@vtsd.com>
2025-01-14 11:46:13 -0500

    basic trig filters

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-import math as math
+# Import statements cus I need those
+import math as m
 import turtle as trtl
 import re
+from lib2to3.pgen2.literals import evalString
 
+filter_list = [r'(sine|sin)\(?', r'(cosine|cos)\(?', r'(tangent|tan)\(?', r'e\^\(?']
+replace_list = ['m.sin(', 'm.cos(', 'm.tan(', 'm.exp(']
+
+global input
 input = input('Input equation: ')
-filter = 'sin\('
 
-equation = re.sub(filter, 'math.sin(', input)
+def re_replace():
+    global input
+    for filt in range(len(filter_list)):
+        input = re.sub(filter_list[filt], replace_list[filt], input)
+
+re_replace()
+
+for num in range (-500, 500):
+   x = num
+   y = eval(input)
+   print(x, y)
 
 
-print(equation)
+'''
+result = eval(input)
+print(result)
+'''
