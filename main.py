010fda47058cffc94266c5b243070d256cc546a9
AustinG <austin.geisler@vtsd.com>
2025-01-15 13:44:22 -0500

    turtle started, draw_axis function functional, also added comments for future reference and organization (its still showing my lack of organizational skills)

--- main.py
+++ main.py
@@ -1,29 +1,58 @@
-# Import statements cus I need those
+### Import statements cus I need those
 import math as m
 import turtle as trtl
 import re
-from lib2to3.pgen2.literals import evalString
+### Imports End
 
+### Set-Up Turtle Program
+## Pen Set-Up
+pen = trtl.Turtle()
+pen.color('white')
+## Window Set-Up
+wn = trtl.Screen()
+wn.bgcolor('black')
+### End turtle Set-Up
+
+### Lists for filter
 filter_list = [r'(sine|sin)\(?', r'(cosine|cos)\(?', r'(tangent|tan)\(?', r'e\^\(?']
 replace_list = ['m.sin(', 'm.cos(', 'm.tan(', 'm.exp(']
+### End Lists
 
-global input
-input = input('Input equation: ')
+### Globaling variables
+global input, x, y
+### End global
 
+### Defining functions
+# Regex to replace text
 def re_replace():
     global input
-    for filt in range(len(filter_list)):
-        input = re.sub(filter_list[filt], replace_list[filt], input)
+    # Run thru list of filters and replaces them accordingly
+    for id in range(len(filter_list)):
+        input = re.sub(filter_list[id], replace_list[id], input)
+
+def draw_axis():
+    global pen, wn
+    pen.penup()
+    pen.goto(0, -400)
+    pen.pendown()
+    pen.goto(0, 400)
+    pen.penup()
+    pen.goto(-500, 0)
+    pen.pendown()
+    pen.goto(500, 0)
+### End function defining
+
+
+# Ask for input equations
+input = input('Input equation: ')
 
 re_replace()
 
+### FOR TESTING ONLY
 for num in range (-500, 500):
    x = num
    y = eval(input)
    print(x, y)
 
 
-'''
-result = eval(input)
-print(result)
-'''
+wn.mainloop()
