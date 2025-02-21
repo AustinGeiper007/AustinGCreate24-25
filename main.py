4fc52f178e14b7511c85910b6f7eac246bca6095
AustinG <austin.geisler@vtsd.com>
2025-02-21 12:32:17 -0500

    Now asks for number of equations when rectangular

--- main.py
+++ main.py
@@ -171,7 +171,13 @@ wn.bgcolor(whiteboard_color)
 
 print('What type of equation would you like to graph? Rectangular (y=...) or Parametric (x=...; y=...)')
 eq_type = input('R/P: ')
-number_of_eqs = int(input('How many equations are you graphing: '))
-start(eq_type, number_of_eqs)
+if eq_type == 'R' or eq_type == 'r':
+    number_of_eqs = int(input('How many equations are you graphing: '))
+    start(eq_type, number_of_eqs)
+elif eq_type == 'P' or eq_type == 'p':
+    number_of_eqs = 1
+    start(eq_type, number_of_eqs)
+else:
+    print('Please enter a valid input (R or P)')
 
 wn.mainloop()
